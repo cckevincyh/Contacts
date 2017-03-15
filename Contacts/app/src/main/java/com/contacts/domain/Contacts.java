96dc2ebc4a7745b9b2fafbf118d26320447613a7
cckevincyh <835287880@qq.com>
2017-03-15 15:33:02 +0800

    完成主界面显示全部联系人

---
+++ Contacts/app/src/main/java/com/contacts/domain/Contacts.java
@@ -0,0 +1,49 @@
+package com.contacts.domain;
+
+/**
+ * Created by c on 2017/3/15.
+ */
+
+public class Contacts {
+
+    /**
+     * 联系人姓名
+     */
+    private String name;
+
+    /**
+     * 联系电话
+     */
+    private String phone;
+
+
+    /**
+     * 排序字母
+     */
+    private String sortKey;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSortKey() {
+        return sortKey;
+    }
+
+    public void setSortKey(String sortKey) {
+        this.sortKey = sortKey;
+    }
+}
---
+++ Contacts/app/src/main/java/com/contacts/main/MainActivity.java
@@ -0,0 +1,162 @@
+package com.contacts.main;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AlphabetIndexer;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.contacts.adapter.ContactAdapter;
+import com.contacts.domain.Contacts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    /**
+     * 分组的布局
+     */
+    private LinearLayout titleLayout;
+
+    /**
+     * 分组上显示的字母
+     */
+    private TextView title;
+
+    /**
+     * 联系人ListView
+     */
+    private ListView contactsListView;
+
+    /**
+     * 联系人列表适配器
+     */
+    private ContactAdapter adapter;
+
+    /**
+     * 用于进行字母表分组
+     */
+    private AlphabetIndexer indexer;
+
+    /**
+     * 存储所有手机中的联系人
+     */
+    private List<Contacts> contacts = new ArrayList<Contacts>();
+
+    /**
+     * 定义字母表的排序规则
+     */
+    private String alphabet = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    /**
+     * 上次第一个可见元素，用于滚动时记录标识。
+     */
+    private int lastFirstVisibleItem = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        adapter = new ContactAdapter(this, R.layout.contact_item, contacts);
+        titleLayout = (LinearLayout) findViewById(R.id.title_layout);
+        title = (TextView) findViewById(R.id.title);
+        contactsListView = (ListView) findViewById(R.id.contacts_list_view);
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;//查询手机里所有的联系人
+
+
+        //ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME 姓名
+        //ContactsContract.CommonDataKinds.Phone.NUMBER 电话
+        Cursor cursor = getContentResolver().query(uri,
+                new String[] { ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER,"sort_key" }, null, null, "sort_key");
+        if (cursor.moveToFirst()) {
+            do {
+                String name = cursor.getString(0);
+                String phone = cursor.getString(1);
+                String sortKey = getSortKey(cursor.getString(2));
+                Contacts contact = new Contacts();
+                contact.setName(name);
+                contact.setPhone(phone);
+                contact.setSortKey(sortKey);
+                contacts.add(contact);
+            } while (cursor.moveToNext());
+        }
+        startManagingCursor(cursor);
+        indexer = new AlphabetIndexer(cursor, 2, alphabet);
+        adapter.setIndexer(indexer);
+        if (contacts.size() > 0) {
+            setupContactsListView();
+        }
+
+
+
+    }
+
+    /**
+     * 为联系人ListView设置监听事件，根据当前的滑动状态来改变分组的显示位置，从而实现挤压动画的效果。
+     */
+    private void setupContactsListView() {
+        contactsListView.setAdapter(adapter);
+        contactsListView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                                 int totalItemCount) {
+                int section = indexer.getSectionForPosition(firstVisibleItem);
+                int nextSecPosition = indexer.getPositionForSection(section + 1);
+                if (firstVisibleItem != lastFirstVisibleItem) {
+                    ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) titleLayout.getLayoutParams();
+                    params.topMargin = 0;
+                    titleLayout.setLayoutParams(params);
+                    title.setText(String.valueOf(alphabet.charAt(section)));
+                }
+                if (nextSecPosition == firstVisibleItem + 1) {
+                    View childView = view.getChildAt(0);
+                    if (childView != null) {
+                        int titleHeight = titleLayout.getHeight();
+                        int bottom = childView.getBottom();
+                        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) titleLayout
+                                .getLayoutParams();
+                        if (bottom < titleHeight) {
+                            float pushedDistance = bottom - titleHeight;
+                            params.topMargin = (int) pushedDistance;
+                            titleLayout.setLayoutParams(params);
+                        } else {
+                            if (params.topMargin != 0) {
+                                params.topMargin = 0;
+                                titleLayout.setLayoutParams(params);
+                            }
+                        }
+                    }
+                }
+                lastFirstVisibleItem = firstVisibleItem;
+            }
+        });
+
+    }
+
+    /**
+     * 获取sort key的首个字符，如果是英文字母就直接返回，否则返回#。
+     *
+     * @param sortKeyString
+     *            数据库中读取出的sort key
+     * @return 英文字母或者#
+     */
+    private String getSortKey(String sortKeyString) {
+        String key = sortKeyString.substring(0, 1).toUpperCase();
+        if (key.matches("[A-Z]")) {
+            return key;
+        }
+        return "#";
+    }
+}
