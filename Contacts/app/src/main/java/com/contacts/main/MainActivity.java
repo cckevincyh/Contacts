4436ca0692d6c454d68e978e028a361aa223ef6b
cckevincyh <835287880@qq.com>
2017-04-23 20:52:49 +0800

    完成搜索栏模糊查询

--- Contacts/app/src/main/java/com/contacts/main/MainActivity.java
+++ Contacts/app/src/main/java/com/contacts/main/MainActivity.java
@@ -2,7 +2,6 @@ package com.contacts.main;
 
 import android.app.Activity;
 import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -12,23 +11,21 @@ import android.os.Handler;
 import android.os.Message;
 import android.provider.ContactsContract;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AbsListView;
 
 import android.widget.AdapterView;
-import android.widget.AlphabetIndexer;
-import android.widget.LinearLayout;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-import com.contacts.adapter.ContactAdapter;
 import com.contacts.adapter.SortAdapter;
 import com.contacts.domain.Contacts;
 import com.contacts.handle.CharacterParser;
@@ -39,6 +36,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static android.R.attr.editable;
+import static android.R.attr.type;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
+import static com.contacts.main.R.id.sort_key;
 
 
 public class MainActivity extends Activity {
@@ -105,7 +106,7 @@ public class MainActivity extends Activity {
         @Override
         public void handleMessage(Message msg) {
             contacts.clear();//清空数据
-            initViews();
+            initViews(0);
         }
     };
 
@@ -116,15 +117,15 @@ public class MainActivity extends Activity {
     protected void onRestart() {
         super.onRestart();//防止报错 android.app.SuperNotCalledException: Activity
         contacts.clear();//清空数据
-        initViews();
+        initViews(0);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initViews();
-
+        initViews(0);
+        initSearchView();
         contactsListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -279,7 +280,7 @@ public class MainActivity extends Activity {
 
 
 
-    private void initViews() {
+    private void initViews(int chooseType) {
         // 实例化汉字转拼音类
         characterParser = CharacterParser.getInstance();
 
@@ -313,8 +314,12 @@ public class MainActivity extends Activity {
             }
         });
 
-        //获取联系人数据
-        SourceDateList = initData();
+        if(chooseType==0) {
+            //获取联系人数据
+            SourceDateList = initData();
+        }else{
+            SourceDateList = contacts;
+        }
 
 
         // 根据a-z进行排序源数据
@@ -397,6 +402,65 @@ public class MainActivity extends Activity {
 
 
 
+    public void initSearchView(){
+        final EditText edit_search = (EditText) findViewById(R.id.et_search);
+        edit_search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+                String content = edit_search.getText().toString();
+                if(!content.trim().equals("")) {
+                    //内容改变的时候使用此方法
+                    Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;//查询手机里所有的联系人
+
+
+                    Cursor cursor = getContentResolver().query(uri,
+                            new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, "sort_key"}, "display_name like '%"+content+"%' or "+
+                                    ContactsContract.CommonDataKinds.Phone.NUMBER+" like'%"+content+"%'",null, "sort_key");
+                    Log.e("12121",content+"!!");
+
+                    Log.e("12121",cursor.getCount()+"!!");
+
+                    query(cursor);
+                    initViews(1);
+                }else{
+                    contacts.clear();//清除一遍数据
+                    initViews(0);
+                }
+            }
+        });
+    }
+
+
+
+    public List<Contacts> query(Cursor cursor){
+        //先清除数据
+        contacts.clear();
+        if (cursor.moveToFirst()) {
+            do {
+                String name = cursor.getString(0);
+                String phone = cursor.getString(1);
+                String sortKey = getSortKey(cursor.getString(2));
+                Contacts contact = new Contacts();
+                contact.setName(name);
+                contact.setPhone(phone);
+                contact.setSortKey(sortKey);
+                contacts.add(contact);
+            } while (cursor.moveToNext());
+        }
+        return contacts;
+    }
 
 
 
